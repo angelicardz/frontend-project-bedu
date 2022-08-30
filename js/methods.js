45dd04428783048036b7812d2e45466de75e1e95
JuanDEV <juan_aguilar1290@outlook.es>
2022-08-30 12:36:09 -0500

    The method that allows you to search for a recipe by name has been added along with a method that is responsible for creating a list of elements by property name

--- js/methods.js
+++ js/methods.js
@@ -1,25 +1,22 @@
 import {apiCall} from './api/api.js';
 const urlBase = 'https://www.themealdb.com/api/json/v1/1/';
 
-const getByProperty = (meal,property, item) => {
-    if(!!(meal[`${property}${item}`])) return meal[`${property}${item}`];
+ 
+// with this function you can get a list of ingredients, measurements and etc
+// because these don't come in the form of a list
+const getByProperty = (meal, property, limit) => {
+    let data = [];
+    for (let i = 1; i < limit; i++){
+        if(meal[`${property}${i}`] === '') break;
+        else data.push(meal[`${property}${i}`]);
+    }
+    return data;
 }
 
+// Geat meal data
 const getMealByName = async (name, url) => {
     const data = await apiCall({url: `${url}search.php?s=${name}`});
-    const imageUrl = data.meals[0].strMealThumb;
-    const recipeName = data.meals[0].strMeal;
-    const ingredients = [];
-    const measures = [];
-    for(let i = 1; i < 21; i++){
-        let ingredient = getByProperty(data.meals[0], 'strIngredient', i);
-        let measure = getByProperty(data.meals[0], 'strMeasure', i);
-        if(ingredient) ingredients.push(ingredient);
-        if(measure) measures.push(measure);
-    }
-    console.log(ingredients)
-    console.log(measures);
-    console.log(data, imageUrl);
+    return data.meals;
 }
 
-getMealByName('Arrabiata', urlBase);
+console.log(getMealByName('Arrabiata', urlBase).then(data => console.log(data)));
