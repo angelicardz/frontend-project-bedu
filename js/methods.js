1993e810414007f9c9c0b648c987029a8fd6d37c
JuanDEV <juan_aguilar1290@outlook.es>
2022-09-01 09:18:04 -0500

    The getByProperty method has been modified to getRecipe, now it helps us obtain a list of ingredients and measurements from a meal and a method has also been added to obtain a list of meals by the first letter of these.

--- js/methods.js
+++ js/methods.js
@@ -2,13 +2,16 @@ import {apiCall} from './api/api.js';
 const urlBase = 'https://www.themealdb.com/api/json/v1/1/';
 
  
-// with this function you can get a list of ingredients, measurements and etc
+// With this function you can get a list of ingredients and measurements
 // because these don't come in the form of a list
-const getByProperty = (meal, property, limit) => {
+const getRecipe = (meal) => {
     let data = [];
-    for (let i = 1; i < limit; i++){
-        if(meal[`${property}${i}`] === '') break;
-        else data.push(meal[`${property}${i}`]);
+    let numberOfElement = 1;
+    while(meal[`${'strIngredient'}${numberOfElement}`] !== ''){
+        let measure = meal[`${'strMeasure'}${numberOfElement}`];
+        let ingredient = meal[`${'strIngredient'}${numberOfElement}`];
+        data.push(`${measure} - ${ingredient}`);
+        numberOfElement++;
     }
     return data;
 }
@@ -19,4 +22,28 @@ const getMealByName = async (name, url) => {
     return data.meals;
 }
 
-console.log(getMealByName('Arrabiata', urlBase).then(data => console.log(data)));
+// Get all meals by first letter
+const getMealsByFirstLetter = async (letter, url) => {
+    const data = await apiCall({url: `${url}search.php?f=${letter}`});
+    return data.meals;
+}
+
+// MARK: Examples -> 
+
+// Example of how to use getMealByName
+getMealByName('Arrabiata', urlBase).then(data => {
+    console.log(data); 
+    // Example of how to get a list of ingredients and measures
+    console.log(getRecipe(data[0])); 
+});
+
+// Example of the use of getMealsByFirstLetter
+getMealsByFirstLetter('a', urlBase).then(data => console.log(data));
+
+
+// Exports
+export {
+    getMealByName,
+    getMealsByFirstLetter,
+    getRecipe
+}
